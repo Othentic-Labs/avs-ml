0b481315ad35a0816446a677a1ac960a9c69923a
gyan0890 <gyanlakshmi@gmail.com>
2024-07-07 17:05:54 +0200

    Committing Model File

--- Smart_Contracts/ModelSubmissionContract.sol
+++ Smart_Contracts/ModelSubmissionContract.sol
@@ -3,38 +3,50 @@
 pragma solidity ^0.8.0;
 
 contract ModelSubmissionContract {
-
+    
+    uint256 id;
     address public admin = 0x4349807050939f95Aa0C494B496F0a694D20F98E;
     
     struct Model {
+        uint256 id;
         address submitter;
         string pinataHash;
+        string updatedPinataHash;
         uint256 timestamp;
         uint256 submissionFee;
         bool isProcessed;
     }
     
     Model[] public models;
+
+    mapping(uint256 => string) taskOwnerCidMap;
+    mapping(address => mapping(uint256 => string)) taskContributorCidMap;
     
     event ModelSubmitted(address indexed submitter, string pinataHash, uint256 timestamp, uint256 submissionFee);
-    
+    event ContributorModelSubmitted(uint256 indexed taskId, string originalModelHash, string contributorModelHash);
+
     modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can call this function");
+        require(msg.sender == admin, "Only the admin can call this function");
         _;
     }
     
-    function submitModel(string memory _pinataHash) external payable {
+    function submitModelByOwner(string memory _pinataHash) external payable {
         
         require(msg.value > 0, "Submission fee must be greater than 0");
 
+        taskOwnerCidMap[id] = _pinataHash;
+
         models.push(Model({
+            id: id,
             submitter: msg.sender,
             pinataHash: _pinataHash,
+            updatedPinataHash: "",
             timestamp: block.timestamp,
             submissionFee: msg.value,
             isProcessed: false
         }));
-        
+
+        id += 1;
         emit ModelSubmitted(msg.sender, _pinataHash, block.timestamp, msg.value);
     }
     
@@ -63,11 +75,23 @@ contract ModelSubmissionContract {
     }
     
     function withdrawFees() external onlyOwner {
-        payable(owner).transfer(address(this).balance);
+        payable(admin).transfer(address(this).balance);
     }
     
-    function markModelAsProcessed(uint256 _modelIndex) external onlyOwner {
+    function markModelAsProcessed(uint256 _modelIndex) external {
+
         require(_modelIndex < models.length, "Invalid model index");
         models[_modelIndex].isProcessed = true;
     }
+
+    function submitModelByContributor(uint256 index, string memory _pinataHash) public {
+        Model storage model = models[index];
+        require(model.isProcessed == false, "Model is not active");
+        model.updatedPinataHash = _pinataHash;
+        taskContributorCidMap[msg.sender][index] = _pinataHash;
+
+        emit ContributorModelSubmitted(model.id, model.pinataHash, _pinataHash);
+    }
+
+
 }
