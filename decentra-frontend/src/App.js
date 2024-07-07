f96a2a5181b92219f50c928b933608c5df3f7d67
Thomas Turek <thomasturekpersonal@gmail.com>
2024-07-07 15:38:59 +0200

    Frontend Implementation

---
+++ decentra-frontend/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      <h1 className="Title">Decentra AI</h1>
+
+      <h1 className="ModelListTitle">Models</h1>
+
+      <div className="ModelList">
+
+      <div className="Model1"/>
+      
+      </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
