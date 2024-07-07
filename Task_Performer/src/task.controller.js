d6a0140dc10b0ead3ad1d113a5db3b2fbfda1dbf
Thomas Turek <thomasturekpersonal@gmail.com>
2024-07-07 14:59:27 +0200

    Task Performer Got Upgraded

--- Task_Performer/src/task.controller.js
+++ Task_Performer/src/task.controller.js
@@ -1,38 +1,73 @@
 "use strict";
-const { Router } = require("express")
+const { Router } = require("express");
 const CustomError = require("./utils/validateError");
 const CustomResponse = require("./utils/validateResponse");
 const oracleService = require("./oracle.service");
 const dalService = require("./dal.service");
+const Web3 = require('web3');
+const axios = require('axios');
 
-const router = Router()
+const router = Router();
+
+// Initialize Web3 and connect to your Ethereum network
+const web3 = new Web3('YOUR_ETHEREUM_NODE_URL');
+
+// ABI and address of your smart contract
+const contractABI = [/* Your contract ABI here */];
+const contractAddress = 'YOUR_CONTRACT_ADDRESS';
+
+const contract = new web3.eth.Contract(contractABI, contractAddress);
+
+// Listen for the event
+contract.events.ModelSubmitted({
+    fromBlock: 'latest'
+})
+.on('data', async (event) => {
+    const submissionFee = event.returnValues.submissionFee;
+    const pinataHash = event.returnValues.pinataHash;
+
+    if (submissionFee > 0) {
+        try {
+            // Query Pinata for the data
+            const pinataData = await queryPinata(pinataHash);
+            // Process the data (e.g., execute the model)
+            await processModelData(pinataData);
+        } catch (error) {
+            console.error('Error processing model:', error);
+        }
+    }
+})
+.on('error', console.error);
+
+async function queryPinata(hash) {
+    const response = await axios.get(`https://gateway.pinata.cloud/ipfs/${hash}`);
+    return response.data;
+}
+
+async function processModelData(data) {
+    // Implement your model processing logic here
+    console.log('Processing model data:', data);
+}
 
 router.post("/execute", async (req, res) => {
     console.log("Executing task");
 
-    // What is the purpopse of the task definition ? Is it shared accross ?
-    //- just in case they are multiple task different performers can pick in our case just on taks
     try {
-        var taskDefinitionId = Number(req.body.taskDefinitionId) || 0;
-        console.log(`taskDefinitionId: ${taskDefinitionId}`)
-        // he passes his parameter, so his matrix of coefficients from his Liner regression
-        // Now we execute the R2
-        const coefficients = req.body.coefficients
-        // computation of the model. (Here we will have to perfom the NN modrl)
-        const rSquared = oracleService.rSquared(coefficients)
-        // We might need to publish it to IPFS or something similar
-        const proofOfTask =  await dalService.publishJSONToIpfs(rSquared);
+        const taskDefinitionId = Number(req.body.taskDefinitionId) || 0;
+        console.log(`taskDefinitionId: ${taskDefinitionId}`);
+
+        const coefficients = req.body.coefficients;
+        const rSquared = oracleService.rSquared(coefficients);
+        const proofOfTask = await dalService.publishJSONToIpfs(rSquared);
         const data = coefficients;
-        // We send the infos to the attestors to validate the computation.
-        await dalService.sendTask(proofOfTask, data, taskDefinitionId);// (proofOfTask, data, taskDefinitionID) => Sending to the Attestators
-        // ; This also propogate the event.
-        return res.status(200).send(new CustomResponse({proofOfTask: cid, data: data, taskDefinitionId: taskDefinitionId}, "Task executed successfully"));
+
+        await dalService.sendTask(proofOfTask, data, taskDefinitionId);
+
+        return res.status(200).send(new CustomResponse({proofOfTask: proofOfTask, data: data, taskDefinitionId: taskDefinitionId}, "Task executed successfully"));
     } catch (error) {
-        console.log(error)
+        console.log(error);
         return res.status(500).send(new CustomError("Something went wrong", {}));
     }
-})
+});
 
-
-
-module.exports = router
+module.exports = router;
