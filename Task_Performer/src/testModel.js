bd777cca0827074ea56120b03d0f992aefe74e29
adam.siptak <adam.siptak@anthology.com>
2024-07-08 03:08:24 +0200

    fixed model saving and loading

--- Task_Performer/src/testModel.js
+++ Task_Performer/src/testModel.js
@@ -1,24 +1,13 @@
 const tf = require('@tensorflow/tfjs-node');
 const fs = require('fs');
 
-async function loadModel() {
-    // Load the model architecture from the JSON file
-    const modelJson = JSON.parse(fs.readFileSync('model_architecture.json'));
-    const model = await tf.models.modelFromJSON(modelJson);
-
-    // Load the model weights from the JSON file
-    const weightsJson = JSON.parse(fs.readFileSync('model_weights.json'));
-    const weightData = weightsJson.map(w => tf.tensor(w.tensor, w.shape, w.dtype));
-    model.setWeights(weightData);
-
+async function loadAndTestModel() {
+    // Load the model
+    const model = await tf.loadLayersModel('file://./model/model.json');
     console.log('Model loaded');
     
-    return model;
-}
-
-loadModel().then(async (model) => {
     // Test the model with several inputs
-    const inputs = tf.tensor2d([-1, -0.5, 0, 0.5, 1], [5, 1]);  // Create a 2D tensor with shape [5, 1]
+    const inputs = tf.tensor2d([-1, -0.5, 0, 0.5, 1], [5, 1]);
     const predictions = model.predict(inputs);
 
     // Actual squared values
@@ -32,4 +21,4 @@ loadModel().then(async (model) => {
     // Print accuracy
     const rmseValue = await rmse.data();
     console.log(`RMSE: ${rmseValue[0]}`);
-});
+}
