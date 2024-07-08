a164aa47435869c5e2d7128552370907c0707797
Thomas Turek <thomasturekpersonal@gmail.com>
2024-07-08 11:24:41 +0200

    front end update

--- zarathustra-frontend/src/App.js
+++ zarathustra-frontend/src/App.js
@@ -7,7 +7,10 @@ function App() {
   const [pastContributions, setPastContributions] = useState([]);
   const [pastEarnings, setPastEarnings] = useState([]);
   const [walletAddress, setWalletAddress] = useState('');
+  const [pinataAddress, setPinataAddress] = useState('');
   const [signer, setSigner] = useState(null);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [isTrainPopupOpen, setIsTrainPopupOpen] = useState(false);
   const [provider, setProvider] = useState(null);
 
   const contractABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint256","name":"taskId","type":"uint256"},{"indexed":false,"internalType":"string","name":"originalModelHash","type":"string"},{"indexed":false,"internalType":"string","name":"contributorModelHash","type":"string"}],"name":"ContributorModelSubmitted","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"submitter","type":"address"},{"indexed":false,"internalType":"string","name":"pinataHash","type":"string"},{"indexed":false,"internalType":"uint256","name":"timestamp","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"submissionFee","type":"uint256"}],"name":"ModelSubmitted","type":"event"},{"inputs":[],"name":"admin","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getAllModels","outputs":[{"components":[{"internalType":"uint256","name":"id","type":"uint256"},{"internalType":"address","name":"owner","type":"address"},{"internalType":"string","name":"pinataHash","type":"string"},{"internalType":"string","name":"updatedPinataHash","type":"string"},{"internalType":"uint256","name":"timestamp","type":"uint256"},{"internalType":"uint256","name":"submissionFee","type":"uint256"},{"internalType":"bool","name":"isProcessed","type":"bool"}],"internalType":"struct ModelSubmissionContract.Model[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getModelCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_start","type":"uint256"},{"internalType":"uint256","name":"_limit","type":"uint256"}],"name":"getModelsPaginated","outputs":[{"components":[{"internalType":"uint256","name":"id","type":"uint256"},{"internalType":"address","name":"owner","type":"address"},{"internalType":"string","name":"pinataHash","type":"string"},{"internalType":"string","name":"updatedPinataHash","type":"string"},{"internalType":"uint256","name":"timestamp","type":"uint256"},{"internalType":"uint256","name":"submissionFee","type":"uint256"},{"internalType":"bool","name":"isProcessed","type":"bool"}],"internalType":"struct ModelSubmissionContract.Model[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_modelIndex","type":"uint256"}],"name":"markModelAsProcessed","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"models","outputs":[{"internalType":"uint256","name":"id","type":"uint256"},{"internalType":"address","name":"owner","type":"address"},{"internalType":"string","name":"pinataHash","type":"string"},{"internalType":"string","name":"updatedPinataHash","type":"string"},{"internalType":"uint256","name":"timestamp","type":"uint256"},{"internalType":"uint256","name":"submissionFee","type":"uint256"},{"internalType":"bool","name":"isProcessed","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"},{"internalType":"string","name":"_pinataHash","type":"string"}],"name":"submitModelByContributor","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_pinataHash","type":"string"}],"name":"submitModelByOwner","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"withdrawFees","outputs":[],"stateMutability":"nonpayable","type":"function"}];
@@ -22,29 +25,14 @@ function App() {
     try {
       const contractInstance = new ethers.Contract(contractAddress, contractABI, signer);
   
-      // First, get the model count
       const modelCount = await contractInstance.getModelCount();
       console.log('Model count:', modelCount.toString());
-  
-      if (modelCount.eq(0)) {
-        console.log('No models found');
-        setLiveModels([]);
-        return;
-      }
-  
-      // If there are models, fetch them
+
       const response = await contractInstance.getAllModels();
       console.log('Raw response from getAllModels:', response);
-  
-      // Check if the response is an array
-      if (!Array.isArray(response)) {
-        console.log('Response is not an array:', response);
-        setLiveModels([]);
-        return;
-      }
-  
+      
       const formattedModels = response.map(modelData => {
-        // Check if modelData is an object with the expected properties
+
         if (typeof modelData === 'object' && modelData !== null) {
           return {
             id: modelData.id ? modelData.id.toString() : 'N/A',
@@ -52,7 +40,7 @@ function App() {
             pinataHash: modelData.pinataHash || 'N/A',
             updatedPinataHash: modelData.updatedPinataHash || 'N/A',
             timestamp: modelData.timestamp ? modelData.timestamp.toString() : 'N/A',
-            submissionFee: modelData.submissionFee ? ethers.utils.formatEther(modelData.submissionFee) : 'N/A',
+            submissionFee: modelData.submissionFee ? ethers.formatEther(modelData.submissionFee) : 'N/A',
             isProcessed: modelData.isProcessed !== undefined ? modelData.isProcessed : 'N/A'
           };
         } else {
@@ -62,11 +50,10 @@ function App() {
       }).filter(model => model !== null);
   
       console.log('Formatted models:', formattedModels);
-      setLiveModels(formattedModels);
+      setModels(formattedModels);
     } catch (error) {
       console.error('Error fetching live models:', error);
-      // You might want to set an error state here to display to the user
-      setLiveModels([]);
+      setModels([]);
     }
   };
 
@@ -82,7 +69,7 @@ function App() {
         const address = await signer.getAddress();
 
         setWalletAddress(address);
-        fetchLiveModels();
+
       } catch (error) {
         console.error("User denied account access or an error occurred:", error);
       }
@@ -90,6 +77,53 @@ function App() {
       alert('Please install a Web3 wallet like MetaMask or Rainbow Wallet');
     }
   };
+
+  const submitNewModel = async () => {
+    setIsPopupOpen(true);
+  };
+
+  const trainNewModel = async () => {
+    setIsTrainPopupOpen(true);
+  };
+
+  const handleModelSubmission = async () => {
+    if (signer && pinataAddress) {
+      try {
+        const contractInstance = new ethers.Contract(contractAddress, contractABI, signer);
+        const tx = await contractInstance.submitModelByOwner(pinataAddress, { value: ethers.parseEther("0.1") });
+        setIsPopupOpen(false);
+        await tx.wait();
+        const allModels = await contractInstance.getAllModels();
+        setModels(allModels);
+        setPinataAddress('');
+      } catch (error) {
+        console.error("Error submitting new model:", error);
+      }
+    }
+  };
+
+  const handleModelTrain = async () => {
+    if (signer && pinataAddress) {
+      try {
+        const contractInstance = new ethers.Contract(contractAddress, contractABI, signer);
+        const tx = await contractInstance.submitModelByContributor(1, pinataAddress);
+        setIsPopupOpen(false);
+        await tx.wait();
+        const allModels = await contractInstance.getAllModels();
+        setModels(allModels);
+        setIsPopupOpen(false);
+        setPinataAddress('');
+      } catch (error) {
+        console.error("Error training new model:", error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (walletAddress) {
+      fetchLiveModels();
+    }
+  }, [walletAddress]);
 
   useEffect(() => {
 
@@ -108,7 +142,7 @@ function App() {
       dummyEarnings.push(`Earning ${i}`);
     }
 
-    setModels(dummyModels);
+    setLiveModels(dummyModels);
     setPastContributions(dummyContributions);
     setPastEarnings(dummyEarnings);
   }, []);
@@ -117,46 +151,70 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 className="Title">Zarathustra AI ❤️</h1>
-
+  
         <button className={`ConnectWalletButton ${walletAddress ? 'connected' : ''}`} onClick={connectWallet}>
-        {walletAddress ? `${walletAddress}` : 'Connect Wallet'}
-         </button>
+          {walletAddress ? `${walletAddress}` : 'Connect Wallet'}
+        </button>
 
-        <button className="NewModel">Submit New Model</button>
+        {isPopupOpen && (
+          <div className="Popup">
+            <h3>Enter Pinata Address</h3>
+            <input
+              className="inputField"
+              type="text"
+              value={pinataAddress}
+              onChange={(e) => setPinataAddress(e.target.value)}
+            />
+            <button className="SubmitButton" onClick={handleModelSubmission}>Submit</button>
+            <button className="CancelButton" onClick={() => setIsPopupOpen(false)}>Cancel</button>
+          </div>
+        )}
 
+        {isTrainPopupOpen && (
+                  <div className="Popup">
+                    <h3>Enter Pinata Address</h3>
+                    <input
+                      className="inputField"
+                      type="text"
+                      value={pinataAddress}
+                      onChange={(e) => setPinataAddress(e.target.value)}
+                    />
+                    <button className="SubmitButton" onClick={handleModelTrain}>Submit</button>
+                    <button className="CancelButton" onClick={() => setIsTrainPopupOpen(false)}>Cancel</button>
+                  </div>
+                )}
+  
+        <button className="NewModel" onClick={submitNewModel}>Submit New Model</button>
+  
         <h1 className="ModelListTitle">Models to Train</h1>
-        <div className="ModelList">
+        <div className={`ModelList ${walletAddress ? '' : 'blurred'}`}>
           {models.map((model, index) => (
-            <div key={index} className="Model">{model}</div>
-          ))}
-        </div>
-
-        <h1 className="OwnerListTitle">Your Live Models</h1>
-        <div className="OwnerList">
-          {liveModels.map((model, index) => (
             <div key={index} className="Model">
-              <p>ID: {model.id}</p>
-              <p>Owner: {model.owner}</p>
-              <p>Pinata Hash: {model.pinataHash}</p>
-              <p>Updated Pinata Hash: {model.updatedPinataHash}</p>
-              <p>Timestamp: {model.timestamp}</p>
-              <p>Submission Fee: {model.submissionFee}</p>
-              <p>Processed: {model.isProcessed ? 'Yes' : 'No'}</p>
+              <p><strong>ID:</strong> {model.id}</p>
+              <p><strong>Pinata Hash:</strong> {model.pinataHash}</p>
+              <button className="TrainButton" onClick={trainNewModel}>Train</button>
             </div>
           ))}
         </div>
-
-        <h1 className="TrainingListTitle">Your Past Contributions</h1>
-        <div className="PerformerList">
-          {pastContributions.map((pastContributions, index) => (
-            <div key={index} className="Model">{pastContributions}</div>
+  
+        <h1 className="OwnerListTitle">Your Live Models</h1>
+        <div className={`OwnerList ${walletAddress ? '' : 'blurred'}`}>
+        {liveModels.map((contribution, index) => (
+            <div key={index} className="Model">{contribution}</div>
           ))}
         </div>
-
+  
+        <h1 className="TrainingListTitle">Your Past Contributions</h1>
+        <div className={`PerformerList ${walletAddress ? '' : 'blurred'}`}>
+          {pastContributions.map((contribution, index) => (
+            <div key={index} className="Model">{contribution}</div>
+          ))}
+        </div>
+  
         <h1 className="AttestationListTitle">Your Earnings</h1>
-        <div className="AttestationList">
-          {pastEarnings.map((pastEarnings, index) => (
-            <div key={index} className="Model">{pastEarnings}</div>
+        <div className={`AttestationList ${walletAddress ? '' : 'blurred'}`}>
+          {pastEarnings.map((earning, index) => (
+            <div key={index} className="Model">{earning}</div>
           ))}
         </div>
       </header>
