6e8d5838e21f121076567b90027b8c2f457712e8
gyan0890 <gyanlakshmi@gmail.com>
2024-07-08 13:07:44 +0200

    Made changes to task_controllers to make temporary values work

--- AVS_WebAPI/src/validator.service.js
+++ AVS_WebAPI/src/validator.service.js
@@ -7,6 +7,7 @@ async function validate({proofOfTask, pinataHashSubmitter, pinataHashOwner, pina
   try {
       //result of the task computed by the Performer, we could also use query pinata.
       const taskResult = await dalService.getIPfsTask(proofOfTask);
+      console.log("Task Result ----- \n", taskResult)
       // Get pinata data from the owner and submitter
       const pinataDataOwner = await dalService.queryPinata(pinataHashOwner);
       const pinataDataSubmitter = await dalService.queryPinata(pinataHashSubmitter);
@@ -15,12 +16,15 @@ async function validate({proofOfTask, pinataHashSubmitter, pinataHashOwner, pina
       // 1) Compute the submitter rSquared
       var rSquaredSubmitter = oracleService.rSquared(pinataDataSubmitter, pinataDataArchitechture, false)
 
-      if(rSquaredSubmitter === taskResult.rSquaredSubmitter){
+      console.log("LHS ------", rSquaredSubmitter, typeof(rSquaredSubmitter))
+      console.log("RHS ------", taskResult.rSquaredSubmitter, typeof(taskResult.rSquaredSubmitter))
+      if(rSquaredSubmitter === parseInt(taskResult.rSquaredSubmitter)){
           // We continue Validation only if the result of the Submitter computed by the operator is the same result as what the Attestator have.
           // If so we compute the rSQuare of the Owner
+          console.log("Inside the validation logic")
           var rSquaredOwner= oracleService.rSquared(pinataDataOwner, pinataDataArchitechture,true)
           let isApproved = false;
-          if(rSquaredSubmitter>rSquaredOwner){
+          if(rSquaredSubmitter > rSquaredOwner){
               isApproved = true;
           }
           return isApproved
